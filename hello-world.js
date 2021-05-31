c7bf40876ac175df566029876746ad69b9f81e8e
Baker Hasan <hasanbd666@gmail.com>
2021-05-31 12:25:36 +0600

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,41 @@
+class AppNavbar extends HTMLElement {
+    constructor() {
+        super();
+        const _style = document.createElement('style');
+        const _template = document.createElement('template');
 
+        _style.innerHTML = `
+        h1 {
+          color: tomato;
+        }
+        `;
+
+        _template.innerHTML = `
+        <h1>
+          Hello world. This is my first custom element
+        </h1>
+      `;
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(_style);
+        this.shadowRoot.appendChild(_template.content.cloneNode(true));
+    }
+
+    static get observedAttributes() {
+        return ["theme"];
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.table({name, oldVal, newVal});
+    }
+
+    connectedCallback() {
+        console.log("Element added to the dom");
+    }
+
+    disconnectedCallback() {
+        console.log("Element removed from dom")
+    }
+}
+
+window.customElements.define('app-navbar', AppNavbar);
